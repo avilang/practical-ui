d54478d7271a2311484e7e3c0c6631861c4d9cf7
avilang <1985945885@qq.com>
2021-05-07 14:07:50 +0800

    button 组件新增 danger prop

---
+++ src/gatsby-theme-docz/wrapper.js
@@ -0,0 +1,9 @@
+export default ({ children }) => (
+  <div
+    style={{
+      fontFamily: 'tahoma,arial,Microsoft YaHei,Hiragino Sans GB,"\u5b8b\u4f53",sans-serif'
+    }}
+  >
+    {children}
+  </div>
+);
