3550acf3842277f8b21c95ed5b4dbad7eae2bdab
avilang <1985945885@qq.com>
2021-05-18 10:52:10 +0800

    引入 rollup

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: {
+    button: 'lib/button/index.js',
+    icon: 'lib/icon/index.js'
+  },
+  output: {
+    dir: 'dist',
+    entryFileNames: '[name]/index.js',
+    format: 'es'
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs({ include: /node_modules/ }),
+    postcss(),
+    babel({
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+      exclude: 'node_modules/**'
+      // plugins: ['@babel/plugin-transform-runtime']
+    })
+  ],
+  external: ['react', 'prop-types']
+};
