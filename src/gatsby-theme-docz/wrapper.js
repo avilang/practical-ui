c4b83b6a18011411b82d4522e297bc356a5531bf
avilang <1985945885@qq.com>
2022-06-21 17:13:53 +0800

    删除所有文件

--- src/gatsby-theme-docz/wrapper.js
+++
@@ -1,9 +0,0 @@
-export default ({ children }) => (
-  <div
-    style={{
-      fontFamily: 'tahoma,arial,Microsoft YaHei,Hiragino Sans GB,"\u5b8b\u4f53",sans-serif'
-    }}
-  >
-    {children}
-  </div>
-);
