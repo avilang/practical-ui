a8636107f53c07cfb5ca9434a702d479196d001c
avilang <avilang@local>
2024-10-22 10:19:04 +0800

    修改组件 build 配置

--- packages/components/vite.config.js
+++ packages/components/vite.config.js
@@ -2,10 +2,20 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import libAssets from '@laynezh/vite-plugin-lib-assets'
 
+// READING https://github.com/vitejs/vite/issues/3295
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    libAssets({
+      include: /\.woff2(\?.*)?$/,
+      name: '[name].[ext]'
+    })
+  ],
   build: {
+    assetsInlineLimit: 0,
+    cssCodeSplit: false,
     lib: {
       entry: resolve(__dirname, 'index.js'),
       formats: ['es'],
